5856d3fda25917c41ee0f68a136bb4c8b1d23de9
KAN KATPARK <kan_k@cmu.ac.th>
2023-12-11 17:51:11 +0700

    handon 3

---
+++ util.py
@@ -0,0 +1,13 @@
+import string
+
+import pandas as pd
+
+def get_and_clean_data():
+    data = pd.read_csv('resource/software_developer_united_states_1971_20191023_1.csv')
+    description = data['job_description']
+    cleaned_description = description.apply(lambda s: s.translate(str.maketrans('', '', string.punctuation + u'\xa0')))
+    cleaned_description = cleaned_description.apply(lambda s: s.lower())
+    cleaned_description = cleaned_description.apply(
+        lambda s: s.translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), '')))
+    cleaned_description = cleaned_description.drop_duplicates()
+    return cleaned_description
